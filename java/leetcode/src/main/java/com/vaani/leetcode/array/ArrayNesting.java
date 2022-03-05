86ba381dd0ca2127f82f912827f1ba68e4e4734f
Kinshuk <kinshuk.ram@gmail.com>
2022-03-05 23:22:47 +0100

    Update ArrayNesting.java

--- java/leetcode/src/main/java/com/vaani/leetcode/array/ArrayNesting.java
+++ java/leetcode/src/main/java/com/vaani/leetcode/array/ArrayNesting.java
@@ -4,25 +4,41 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * 09/10/2019 A zero-indexed array A of length N contains all
- * integers from 0 to N-1. Find and return the longest length of set S, where S[i] = {A[i], A[A[i]],
- * A[A[A[i]]], ... } subjected to the rule below.
+ * https://leetcode.com/problems/array-nesting/
  *
- * <p>Suppose the first element in S starts with the selection of element A[i] of index = i, the
- * next element in S should be A[A[i]], and then A[A[A[i]]]… By that analogy, we stop adding right
- * before a duplicate element occurs in S.
+ * 565. Array Nesting
+ * Medium
  *
- * <p>Example 1:
+ * You are given an integer array nums of length n where nums is a permutation of the numbers in the range [0, n - 1].
  *
- * <p>Input: A = [5,4,0,3,1,6,2] Output: 4 Explanation: A[0] = 5, A[1] = 4, A[2] = 0, A[3] = 3, A[4]
- * = 1, A[5] = 6, A[6] = 2.
+ * You should build a set s[k] = {nums[k], nums[nums[k]], nums[nums[nums[k]]], ... } subjected to the following rule:
  *
- * <p>One of the longest S[K]: S[0] = {A[0], A[5], A[6], A[2]} = {5, 6, 2, 0}
+ * The first element in s[k] starts with the selection of the element nums[k] of index = k.
+ * The next element in s[k] should be nums[nums[k]], and then nums[nums[nums[k]]], and so on.
+ * We stop adding right before a duplicate element occurs in s[k].
+ * Return the longest length of a set s[k].
  *
- * <p>Note:
  *
- * <p>N is an integer within the range [1, 20,000]. The elements of A are all distinct. Each element
- * of A is an integer within the range [0, N-1].
+ *
+ * Example 1:
+ *
+ * Input: nums = [5,4,0,3,1,6,2]
+ * Output: 4
+ * Explanation:
+ * nums[0] = 5, nums[1] = 4, nums[2] = 0, nums[3] = 3, nums[4] = 1, nums[5] = 6, nums[6] = 2.
+ * One of the longest sets s[k]:
+ * s[0] = {nums[0], nums[5], nums[6], nums[2]} = {5, 6, 2, 0}
+ * Example 2:
+ *
+ * Input: nums = [0,1,2]
+ * Output: 1
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= nums.length <= 10^5
+ * 0 <= nums[i] < nums.length
+ * All the values of nums are unique.
  */
 public class ArrayNesting {
     public static void main(String[] args) {
@@ -30,11 +46,10 @@ public class ArrayNesting {
         System.out.println(new ArrayNesting().arrayNesting(A));
     }
 
-    Set<Integer> done;
-    int count;
+    private final Set<Integer> done = new HashSet<>();
+    private int count;
 
     public int arrayNesting(int[] nums) {
-        done = new HashSet<>();
         int max = 0;
         for (int i = 0; i < nums.length; i++) {
             if (!done.contains(i)) {
