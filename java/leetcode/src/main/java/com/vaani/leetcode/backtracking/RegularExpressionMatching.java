0e26aece1c31d7c6713b8b6834da06c700512805
Kinshuk <kinshuk.ram@gmail.com>
2022-03-11 23:24:02 +0100

    Update RegularExpressionMatching.java

--- java/leetcode/src/main/java/com/vaani/leetcode/backtracking/RegularExpressionMatching.java
+++ java/leetcode/src/main/java/com/vaani/leetcode/backtracking/RegularExpressionMatching.java
@@ -68,4 +68,32 @@ public class RegularExpressionMatching {
             }
         }
     }
+
+    static class UsingRecursion {
+        public boolean isMatch(String s, String p) {
+            return backtrack(s, p, 0, 0);
+        }
+
+        private boolean backtrack(String s, String p, int i, int j) {
+            int sn = s.length(), pn = p.length();
+            if (j == pn) { // since * in p can match 0 of previous char, so empty string(i==sn) may match p
+                return i == sn;
+            }
+            char pj = p.charAt(j);
+            if (j + 1 < pn && p.charAt(j + 1) == '*') { //match *, needs to look at the next char to repeat current char
+                if (backtrack(s, p, i, j + 2)) {
+                    return true;
+                }
+                while (i < sn && (pj == '.' || pj == s.charAt(i))) {
+                    if (backtrack(s, p, ++i, j + 2)) {
+                        return true;
+                    }
+                }
+            } else if (i < sn && (s.charAt(i) == pj ||    //match char
+                    pj == '.')) {              //match dot
+                return backtrack(s, p, i + 1, j + 1);
+            }
+            return false;
+        }
+    }
 }
