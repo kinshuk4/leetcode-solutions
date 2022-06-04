d8061028f5c0ec21f5bd9bc76dab010bc6b37b56
Kinshuk <kinshuk.ram@gmail.com>
2022-06-05 00:50:26 +0200

    Update ReachableNodesInSubdividedGraph.java

--- java/leetcode/src/main/java/com/vaani/leetcode/heap/ReachableNodesInSubdividedGraph.java
+++ java/leetcode/src/main/java/com/vaani/leetcode/heap/ReachableNodesInSubdividedGraph.java
@@ -3,56 +3,61 @@ package com.vaani.leetcode.heap;
 import java.util.*;
 
 /**
- * 07/05/2019 Starting with an undirected graph (the "original
- * graph") with nodes from 0 to N-1, subdivisions are made to some of the edges.
- *
- * <p>The graph is given as follows: edges[k] is a list of integer pairs (i, j, n) such that (i, j)
- * is an vertex of the original graph,
- *
- * <p>and n is the total number of new nodes on that vertex.
- *
- * <p>Then, the vertex (i, j) is deleted from the original graph, n new nodes (x_1, x_2, ..., x_n) are
- * added to the original graph,
- *
- * <p>and n+1 new edges (i, x_1), (x_1, x_2), (x_2, x_3), ..., (x_{n-1}, x_n), (x_n, j) are added to
- * the original graph.
- *
- * <p>Now, you start at node 0 from the original graph, and in each move, you travel along one vertex.
- *
- * <p>Return how many nodes you can reach in at most M moves.
- *
- * <p>Example 1:
- *
- * <p>Input: edges = [[0,1,10],[0,2,1],[1,2,2]], M = 6, N = 3 Output: 13 Explanation: The nodes that
- * are reachable in the final graph after M = 6 moves are indicated below.
- *
- * <p>Example 2:
- *
- * <p>Input: edges = [[0,1,4],[1,2,6],[0,2,8],[1,3,1]], M = 10, N = 4 Output: 23
- *
- * <p>Note:
- *
- * <p>0 <= edges.length <= 10000 0 <= edges[i][0] < edges[i][1] < N There does not exist any i != j
- * for which edges[i][0] == edges[j][0] and edges[i][1] == edges[j][1]. The original graph has no
- * parallel edges. 0 <= edges[i][2] <= 10000 0 <= M <= 10^9 1 <= N <= 3000 A reachable node is a
- * node that can be travelled to using at most M moves starting from node 0.
- *
- * <p>Solution: O(E log N) E is the length of edges and N is the number of nodes. The n nodes on a
- * vertex form a weight and thus the graph becomes a weighted graph. Keep track of number of moves
- * available and run a Dijkstra's algorithm.
+ * https://leetcode.com/problems/reachable-nodes-in-subdivided-graph/
+ * 882. Reachable Nodes In Subdivided Graph
+ * Hard
+ * <p>
+ * You are given an undirected graph (the "original graph") with n nodes labeled from 0 to n - 1. You decide to subdivide each edge in the graph into a chain of nodes, with the number of new nodes varying between each edge.
+ * <p>
+ * The graph is given as a 2D array of edges where edges[i] = [ui, vi, cnti] indicates that there is an edge between nodes ui and vi in the original graph, and cnti is the total number of new nodes that you will subdivide the edge into. Note that cnti == 0 means you will not subdivide the edge.
+ * <p>
+ * To subdivide the edge [ui, vi], replace it with (cnti + 1) new edges and cnti new nodes. The new nodes are x1, x2, ..., xcnti, and the new edges are [ui, x1], [x1, x2], [x2, x3], ..., [xcnti+1, xcnti], [xcnti, vi].
+ * <p>
+ * In this new graph, you want to know how many nodes are reachable from the node 0, where a node is reachable if the distance is maxMoves or less.
+ * <p>
+ * Given the original graph and maxMoves, return the number of nodes that are reachable from node 0 in the new graph.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * <p>
+ * Input: edges = [[0,1,10],[0,2,1],[1,2,2]], maxMoves = 6, n = 3
+ * Output: 13
+ * Explanation: The edge subdivisions are shown in the image above.
+ * The nodes that are reachable are highlighted in yellow.
+ * Example 2:
+ * <p>
+ * Input: edges = [[0,1,4],[1,2,6],[0,2,8],[1,3,1]], maxMoves = 10, n = 4
+ * Output: 23
+ * Example 3:
+ * <p>
+ * Input: edges = [[1,2,4],[1,4,5],[1,3,1],[2,3,4],[3,4,5]], maxMoves = 17, n = 5
+ * Output: 1
+ * Explanation: Node 0 is disconnected from the rest of the graph, so only node 0 is reachable.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 0 <= edges.length <= min(n * (n - 1) / 2, 10^4)
+ * edges[i].length == 3
+ * 0 <= u_i < v_i < n
+ * There are no multiple edges in the graph.
+ * 0 <= cnt_i <= 10^4
+ * 0 <= maxMoves <= 10^9
+ * 1 <= n <= 3000
  */
 public class ReachableNodesInSubdividedGraph {
-
-    /**
-     * Main method
-     *
-     * @param args
-     */
     public static void main(String[] args) {
         int[][] edges = {{0, 1, 1000}, {0, 2, 1}, {1, 2, 1}};
         System.out.println(new ReachableNodesInSubdividedGraph().reachableNodes(edges, 200, 3));
     }
 
+    /**
+     * <p>Solution: O(E log N) E is the length of edges and N is the number of nodes. The n nodes on a
+     * vertex form a weight and thus the graph becomes a weighted graph. Keep track of number of moves
+     * available and run a Dijkstra's algorithm.
+     */
     static class Node {
         int n, w;
 
@@ -62,7 +67,7 @@ public class ReachableNodesInSubdividedGraph {
         }
     }
 
-    public int reachableNodes(int[][] edges, int M, int N) {
+    public int reachableNodes(int[][] edges, int maxMoves, int n) {
         Map<Integer, List<Node>> graph = new HashMap<>();
         for (int[] e : edges) {
             graph.putIfAbsent(e[0], new ArrayList<>());
@@ -85,7 +90,7 @@ public class ReachableNodesInSubdividedGraph {
                 if (children != null) {
                     for (Node c : children) {
                         if (!distance.containsKey(c.n)) {
-                            int availableMoves = M - curr.w;
+                            int availableMoves = maxMoves - curr.w;
                             int nodesInBetween = c.w - 1;
                             if (nodesInBetween >= availableMoves) {
                                 count += availableMoves;
@@ -97,15 +102,12 @@ public class ReachableNodesInSubdividedGraph {
                                 }
                             }
                         } else {
-                            int childAvailableMoves = M - distance.get(c.n);
+                            int childAvailableMoves = maxMoves - distance.get(c.n);
                             int nodesInBetween = c.w - 1;
                             int unvisitedNodes = nodesInBetween - childAvailableMoves;
                             if (unvisitedNodes > 0) {
-                                int availableMovesForCurr = M - distance.get(curr.n);
-                                count +=
-                                        (unvisitedNodes >= availableMovesForCurr
-                                                ? availableMovesForCurr
-                                                : unvisitedNodes);
+                                int availableMovesForCurr = maxMoves - distance.get(curr.n);
+                                count += Math.min(unvisitedNodes, availableMovesForCurr);
                             }
                         }
                     }
