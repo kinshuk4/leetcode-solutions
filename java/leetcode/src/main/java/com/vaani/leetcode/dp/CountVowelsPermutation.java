9f1fcd5c5c7263561ccde804c08262071c23a4f1
Kinshuk <kinshuk.ram@gmail.com>
2022-07-09 00:55:42 +0200

    Add some pnc problems

--- java/leetcode/src/main/java/com/vaani/leetcode/dp/CountVowelsPermutation.java
+++ java/leetcode/src/main/java/com/vaani/leetcode/dp/CountVowelsPermutation.java
@@ -4,31 +4,49 @@ import java.util.*;
 
 /**
  * https://leetcode.com/problems/count-vowels-permutation/
- * Given an integer n, your task is to count how many
- * strings of length n can be formed under the following rules:
- *
- * <p>Each character is a lower case vowel ('a', 'e', 'i', 'o', 'u') Each vowel 'a' may only be
- * followed by an 'e'. Each vowel 'e' may only be followed by an 'a' or an 'i'. Each vowel 'i' may
- * not be followed by another 'i'. Each vowel 'o' may only be followed by an 'i' or a 'u'. Each
- * vowel 'u' may only be followed by an 'a'. Since the answer may be too large, return it modulo
- * 10^9 + 7.
- *
- * <p>Example 1:
- *
- * <p>Input: n = 1 Output: 5 Explanation: All possible strings are: "a", "e", "i" , "o" and "u".
+ * 1220. Count Vowels Permutation
+ * <p>
+ * Given an integer n, your task is to count how many strings of length n can be formed under the following rules:
+ * <p>
+ * Each character is a lower case vowel ('a', 'e', 'i', 'o', 'u')
+ * Each vowel 'a' may only be followed by an 'e'.
+ * Each vowel 'e' may only be followed by an 'a' or an 'i'.
+ * Each vowel 'i' may not be followed by another 'i'.
+ * Each vowel 'o' may only be followed by an 'i' or a 'u'.
+ * Each vowel 'u' may only be followed by an 'a'.
+ * Since the answer may be too large, return it modulo 10^9 + 7.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 1
+ * Output: 5
+ * Explanation: All possible strings are: "a", "e", "i" , "o" and "u".
  * Example 2:
- *
- * <p>Input: n = 2 Output: 10 Explanation: All possible strings are: "ae", "ea", "ei", "ia", "ie",
- * "io", "iu", "oi", "ou" and "ua". Example 3:
- *
- * <p>Input: n = 5 Output: 68
+ * <p>
+ * Input: n = 2
+ * Output: 10
+ * Explanation: All possible strings are: "ae", "ea", "ei", "ia", "ie", "io", "iu", "oi", "ou" and "ua".
+ * Example 3:
+ * <p>
+ * Input: n = 5
+ * Output: 68
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= n <= 2 * 10^4
  */
 public class CountVowelsPermutation {
     public static void main(String[] args) {
     }
 
     public int countVowelPermutation(int n) {
-        if (n == 1) return 5;
+        if (n == 1) {
+            return 5;
+        }
+        final int MOD = 1000000007;
         Map<Character, List<Character>> graph = new HashMap<>();
         List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u');
         Map<Character, Integer> count = new HashMap<>();
@@ -55,7 +73,7 @@ public class CountVowelsPermutation {
             for (char c : vowels) {
                 List<Character> children = graph.get(c);
                 for (char child : children) {
-                    charCount[j] = ((charCount[j] + count.get(child)) % 1000000007);
+                    charCount[j] = ((charCount[j] + count.get(child)) % MOD);
                 }
                 j++;
             }
@@ -66,7 +84,7 @@ public class CountVowelsPermutation {
         }
         int sum = 0;
         for (char c : vowels) {
-            sum = ((sum + (count.get(c))) % 1000000007);
+            sum = ((sum + (count.get(c))) % MOD);
         }
         return sum;
     }
--- java/leetcode/src/main/java/com/vaani/leetcode/dp/DecodeWaysII.java
+++ java/leetcode/src/main/java/com/vaani/leetcode/dp/DecodeWaysII.java
@@ -1,59 +1,96 @@
 package com.vaani.leetcode.dp;
 
 /**
- * 11/12/2017. A message containing letters from A-Z is being
- * encoded to numbers using the following mapping way:
- *
- * <p>'A' -> 1 'B' -> 2 ... 'Z' -> 26 Beyond that, now the encoded string can also contain the
- * character '*', which can be treated as one of the numbers from 1 to 9.
- *
- * <p>Given the encoded message containing digits and the character '*', return the total number of
- * ways to decode it.
- *
- * <p>Also, since the answer may be very large, you should return the output mod 109 + 7.
- *
- * <p>Example 1: Input: "*" Output: 9 Explanation: The encoded message can be decoded to the com.vaani.leetcode.string:
- * "A", "B", "C", "D", "E", "F", "G", "H", "I". Example 2: Input: "1*" Output: 9 + 9 = 18 Note: The
- * length of the input string will fit in range [1, 105]. The input string will only contain the
- * character '*' and digits '0' - '9'.
- *
- * <p>Solution: O(n) consider each digit and a pair of digits and perform a cartesian product to
- * calculate the total number of ways. A pair of digits are to be considered only if their combined
- * value does not exceed 26. Corner cases with combination of * and 0s can be tricky
+ * https://leetcode.com/problems/decode-ways-ii/
+ * 639. Decode Ways II
+ * Hard
+ * <p>
+ * 753
+ * <p>
+ * 666
+ * <p>
+ * Add to List
+ * <p>
+ * Share
+ * A message containing letters from A-Z can be encoded into numbers using the following mapping:
+ * <p>
+ * 'A' -> "1"
+ * 'B' -> "2"
+ * ...
+ * 'Z' -> "26"
+ * To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+ * <p>
+ * "AAJF" with the grouping (1 1 10 6)
+ * "KJF" with the grouping (11 10 6)
+ * Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+ * <p>
+ * In addition to the mapping above, an encoded message may contain the '*' character, which can represent any digit from '1' to '9' ('0' is excluded). For example, the encoded message "1*" may represent any of the encoded messages "11", "12", "13", "14", "15", "16", "17", "18", or "19". Decoding "1*" is equivalent to decoding any of the encoded messages it can represent.
+ * <p>
+ * Given a string s containing digits and the '*' character, return the number of ways to decode it.
+ * <p>
+ * Since the answer may be very large, return it modulo 109 + 7.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "*"
+ * Output: 9
+ * Explanation: The encoded message can represent any of the encoded messages "1", "2", "3", "4", "5", "6", "7", "8", or "9".
+ * Each of these can be decoded to the strings "A", "B", "C", "D", "E", "F", "G", "H", and "I" respectively.
+ * Hence, there are a total of 9 ways to decode "*".
+ * Example 2:
+ * <p>
+ * Input: s = "1*"
+ * Output: 18
+ * Explanation: The encoded message can represent any of the encoded messages "11", "12", "13", "14", "15", "16", "17", "18", or "19".
+ * Each of these encoded messages have 2 ways to be decoded (e.g. "11" can be decoded to "AA" or "K").
+ * Hence, there are a total of 9 * 2 = 18 ways to decode "1*".
+ * Example 3:
+ * <p>
+ * Input: s = "2*"
+ * Output: 15
+ * Explanation: The encoded message can represent any of the encoded messages "21", "22", "23", "24", "25", "26", "27", "28", or "29".
+ * "21", "22", "23", "24", "25", and "26" have 2 ways of being decoded, but "27", "28", and "29" only have 1 way.
+ * Hence, there are a total of (6 * 2) + (3 * 1) = 12 + 3 = 15 ways to decode "2*".
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= s.length <= 105
+ * s[i] is a digit or '*'.
  */
 public class DecodeWaysII {
 
-    private final int CONST = 1000000007;
-    private int[] dp;
-
-    /**
-     * Main method
-     *
-     * @param args
-     * @throws Exception
-     */
     public static void main(String[] args) throws Exception {
         System.out.println(new DecodeWaysII().numDecodings("10"));
     }
 
+    /**
+     * <p>Solution: O(n) consider each digit and a pair of digits and perform a cartesian product to
+     * calculate the total number of ways. A pair of digits are to be considered only if their combined
+     * value does not exceed 26. Corner cases with combination of * and 0s can be tricky
+     */
     public int numDecodings(String s) {
-        dp = new int[s.length() + 1];
-        if (s.charAt(s.length() - 1) == '*') {
-            dp[s.length() - 1] = 9;
-        } else if (s.charAt(s.length() - 1) == '0') {
-            dp[s.length() - 1] = 0;
-        } else dp[s.length() - 1] = 1;
-        dp[s.length()] = 1;
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        final int CONST = 1000000007;
+        if (s.charAt(n - 1) == '*') {
+            dp[n - 1] = 9;
+        } else if (s.charAt(n - 1) == '0') {
+            dp[n - 1] = 0;
+        } else dp[n - 1] = 1;
+        dp[n] = 1;
 
-        for (int i = s.length() - 2; i >= 0; i--) {
+        for (int i = n - 2; i >= 0; i--) {
             char curr = s.charAt(i);
             char next = s.charAt(i + 1);
+
             switch (curr) {
-                case '0':
-                    dp[i] = 0;
-                    break;
+                case '0' -> dp[i] = 0;
+
                 // number begins with a '*'
-                case '*':
+                case '*' -> {
                     dp[i] = (int) ((9 * (long) dp[i + 1]) % CONST);
                     switch (next) {
                         // The next char is a '*'
@@ -77,27 +114,22 @@ public class DecodeWaysII {
                             }
                             break;
                     }
-                    break;
-
-                default:
+                }
+                default -> {
                     dp[i] = dp[i + 1];
-                    switch (next) {
-                        case '*':
-                            if ((curr - '0') == 1) {
-                                dp[i] = (int) ((dp[i] + ((9 * (long) dp[i + 2]) % CONST)) % CONST);
-                            } else if ((curr - '0') == 2) {
-                                dp[i] = (int) ((dp[i] + ((6 * (long) dp[i + 2]) % CONST)) % CONST);
-                            }
-                            break;
-
-                        default:
-                            if ((curr - '0') == 1) {
-                                dp[i] = ((dp[i] + dp[i + 2]) % CONST);
-                            } else if ((curr - '0') == 2 && (next - '0' <= 6)) {
-                                dp[i] = ((dp[i] + dp[i + 2]) % CONST);
-                            }
-                            break;
+                    if (next == '*') {
+                        if ((curr - '0') == 1) {
+                            dp[i] = (int) ((dp[i] + ((9 * (long) dp[i + 2]) % CONST)) % CONST);
+                        } else if ((curr - '0') == 2) {
+                            dp[i] = (int) ((dp[i] + ((6 * (long) dp[i + 2]) % CONST)) % CONST);
+                        }
+                    } else {
+                        if ((curr - '0') == 1 || ((curr - '0') == 2 && (next - '0' <= 6))) {
+                            dp[i] = (dp[i] + dp[i + 2]) % CONST;
+                            ;
+                        }
                     }
+                }
             }
         }
         return dp[0];
